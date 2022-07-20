bedadea5928d42ac0f96554660f94bfb0d7a6080
Michael Eckert <mi.eckert@gmx.de>
2022-07-20 15:58:24 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.post("/echo", function(req, res) {
+    console.log("Raw Headers:");
+    console.log(req.rawHeaders);
+    console.log("Headers:");
+    console.log(req.headers)
+    console.log("Request:");
+    console.log(req.body);    
+    res.send(req.body);
+})
+
+var server = http.createServer(app);
+server.listen(port);
